95d63acc9271c834d1533a8fe9b838dcb325df77
kavindisilva <48406613+kavindisilva@users.noreply.github.com>
2026-03-23 17:09:17 +0000

    adding new rest assured project (#4)

---
+++ RestAss26Test/src/main/java/RestAssured26Test/RestAss26Test/App.java
@@ -0,0 +1,10 @@
+package RestAssured26Test.RestAss26Test;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
