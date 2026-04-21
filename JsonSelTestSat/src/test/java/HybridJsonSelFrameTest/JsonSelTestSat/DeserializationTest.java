f25bf9e23f17a18bb4134ddc6fab695fc205e414
udarajayawardhane <udarajayawardhane@udaras-air.broadband>
2026-04-21 11:59:56 +0100

    changed beforemethod of each test classes to enable headless chrome run.

--- JsonSelTestSat/src/test/java/HybridJsonSelFrameTest/JsonSelTestSat/DeserializationTest.java
+++ JsonSelTestSat/src/test/java/HybridJsonSelFrameTest/JsonSelTestSat/DeserializationTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -44,12 +45,31 @@ new RestAssured();
   }
   @BeforeMethod
   public void beforeMethod() {
-	  WebDriverManager.chromedriver().setup();
-	  driver = new ChromeDriver();
-	  driver.manage().window().maximize();
-	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-	  driver.get("https://www.saucedemo.com/");
+//	  WebDriverManager.chromedriver().setup();
+//	  driver = new ChromeDriver();
+//	  driver.manage().window().maximize();
+//	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+//	  driver.get("https://www.saucedemo.com/");
 
+	  WebDriverManager.chromedriver().setup();
+
+	    ChromeOptions options = new ChromeOptions();
+
+	    // Detect CI environment (GitHub Actions sets this automatically)
+	    String ci = System.getenv("CI");
+
+	    if (ci != null && ci.equalsIgnoreCase("true")) {
+	        options.addArguments("--headless=new");
+	        options.addArguments("--no-sandbox");
+	        options.addArguments("--disable-dev-shm-usage");
+	        options.addArguments("--disable-gpu");
+	    }
+
+	    driver = new ChromeDriver(options);
+
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	    driver.get("https://www.saucedemo.com/");
 
   }
 
--- JsonSelTestSat/src/test/java/TestFileJson/JsonTest.java
+++ JsonSelTestSat/src/test/java/TestFileJson/JsonTest.java
@@ -1,9 +1,11 @@
 package TestFileJson;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -41,10 +43,30 @@ public class JsonTest {
   }
   @BeforeMethod
   public void beforeMethod() {
+//	  WebDriverManager.chromedriver().setup();
+//	  driver = new ChromeDriver();
+//	  driver.manage().window().maximize();
+//	  driver.get("https://www.saucedemo.com/");
+	  
 	  WebDriverManager.chromedriver().setup();
-	  driver = new ChromeDriver();
-	  driver.manage().window().maximize();
-	  driver.get("https://www.saucedemo.com/");
+
+	    ChromeOptions options = new ChromeOptions();
+
+	    // Detect CI environment (GitHub Actions sets this automatically)
+	    String ci = System.getenv("CI");
+
+	    if (ci != null && ci.equalsIgnoreCase("true")) {
+	        options.addArguments("--headless=new");
+	        options.addArguments("--no-sandbox");
+	        options.addArguments("--disable-dev-shm-usage");
+	        options.addArguments("--disable-gpu");
+	    }
+
+	    driver = new ChromeDriver(options);
+
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	    driver.get("https://www.saucedemo.com/");
 
   }
 
