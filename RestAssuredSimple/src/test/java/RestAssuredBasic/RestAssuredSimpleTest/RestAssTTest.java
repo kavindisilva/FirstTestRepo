19a1320c8fa813b6eb36742977a79d9227b11d0a
udarajayawardhane <udarajayawardhane@udaras-air.broadband>
2026-04-21 15:05:35 +0100

    added lines to run chrome in headless mode

--- RestAssuredSimple/src/test/java/RestAssuredBasic/RestAssuredSimpleTest/RestAssTTest.java
+++ RestAssuredSimple/src/test/java/RestAssuredBasic/RestAssuredSimpleTest/RestAssTTest.java
@@ -1,8 +1,11 @@
 package RestAssuredBasic.RestAssuredSimpleTest;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,11 +28,31 @@ public class RestAssTTest {
 
 @BeforeMethod
 public void beforeMethod() {
-	  WebDriverManager.chromedriver().setup();
-	  driver = new ChromeDriver();
-	  driver.manage().window().maximize();
-	  driver.get("https://www.saucedemo.com/");
-	  
+//	  WebDriverManager.chromedriver().setup();
+//	  driver = new ChromeDriver();
+//	  driver.manage().window().maximize();
+//	 driver.get("https://www.saucedemo.com/");
+//	  
+	
+	 WebDriverManager.chromedriver().setup();
+
+	    ChromeOptions options = new ChromeOptions();
+
+	    // Detect CI environment (GitHub Actions sets this automatically)
+	    String ci = System.getenv("CI");
+
+	    if (ci != null && ci.equalsIgnoreCase("true")) {
+	        options.addArguments("--headless=new");
+	        options.addArguments("--no-sandbox");
+	        options.addArguments("--disable-dev-shm-usage");
+	        options.addArguments("--disable-gpu");
+	    }
+
+	    driver = new ChromeDriver(options);
+
+	    driver.manage().window().maximize();
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+	    driver.get("https://www.saucedemo.com/");
 }
 
 
